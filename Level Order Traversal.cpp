fdfb4d844614e6533bc79ceff2dadcfe6ed2e2a5
Kabir ahuja <ahujakabir13@gmail.com>
2023-06-23 17:23:16 +0530

    Create Level Order Traversal.cpp

---
+++ Level Order Traversal.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h> 
+/************************************************************
+
+    Following is the BinaryTreeNode class structure
+
+    template <typename T>
+    class BinaryTreeNode {
+       public:
+        T val;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T val) {
+            this->val = val;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+vector<int> getLevelOrder(BinaryTreeNode<int> *root)
+{
+    //  Write your code here.
+    
+
+
+    vector<int>res;
+    if(root==NULL)
+       return res;
+    queue<BinaryTreeNode<int> *>q;
+    q.push(root);
+    while(!q.empty())
+    {
+        int n=q.size();
+        for(int i=0;i<n;i++)
+        {
+            BinaryTreeNode<int> *temp=q.front();
+            q.pop();
+            res.push_back(temp->val);
+            if(temp->left)
+            {
+                q.push(temp->left);
+            }
+            if(temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+    return res;
+}
