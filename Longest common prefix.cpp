0b19322b4dd214fadc63d9f3cada61077ac8f42b
Kabir ahuja <ahujakabir13@gmail.com>
2023-06-25 12:40:14 +0530

    Create Longest common prefix.cpp

---
+++ Longest common prefix.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+string longestCommonPrefix(vector<string> &arr, int n)
+{
+    // Write your code here
+    if(n==0)
+       return "";
+
+    string ans="";
+    sort(arr.begin(),arr.end());
+    string a=arr[0];
+    string b=arr[n-1];
+    // cout<<a<<" "<<b;
+    for(int i=0;i<a.size();i++)
+    {
+        if(a[i]==b[i])
+        {
+            ans+=a[i];
+        }
+        else
+        {
+            break;
+        }
+    }
+    return ans;
+}
+
+
