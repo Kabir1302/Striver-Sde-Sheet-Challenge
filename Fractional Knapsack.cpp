d389c0d5de489533d1ec23a9c52d9d8e82fb74fd
Kabir ahuja <ahujakabir13@gmail.com>
2023-06-27 23:10:05 +0530

    Create Fractional Knapsack.cpp

---
+++ Fractional Knapsack.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h> 
+bool cmp(pair<int,int>a,pair<int,int>b)
+{
+    double r1=(double)a.second/(double)a.first;
+    double r2=(double)b.second/(double)b.first;
+    
+    return r1>r2;
+}
+double maximumValue (vector<pair<int, int>>& items, int n, int w)
+{
+    // Write your code here.
+    // ITEMS contains {weight, value} pairs.
+
+    sort(items.begin(),items.end(),cmp);
+    int curr=0;
+    double res=0.0;
+
+    for(int i=0;i<n;i++)
+    {
+        if(curr+items[i].first<=w)
+        {
+            curr+=items[i].first;
+            res+=items[i].second;
+        }
+        else{
+            int rem=w-curr;
+            res+=((double)items[i].second/(double)items[i].first)*(double)rem;
+            break;
+        }
+    }
+    return res;
+}
+
+
